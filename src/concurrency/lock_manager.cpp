96cea2179b8675d498af00c0688f3ff665d64931
Joe Tsai <joe85123@gmail.com>
2022-02-09 00:45:42 +0800

    Fix various bugs

--- src/concurrency/lock_manager.cpp
+++ src/concurrency/lock_manager.cpp
@@ -69,6 +69,9 @@ bool LockManager::LockExclusive(Transaction *txn, const RID &rid) {
     AbortCurrentTransaction(txn);
     return false;
   }
+  if (txn->IsSharedLocked(rid)) {
+    return false;
+  }
   if (txn->IsExclusiveLocked(rid)) {
     return true;
   }
@@ -247,17 +250,36 @@ void LockManager::ProcessQueue(LockRequestQueue *request_queue) {
   auto queue_iter = request_queue->request_queue_.begin();
   while (queue_iter != request_queue->request_queue_.end()) {
     if (queue_iter->lock_mode_ == LockMode::SHARED) {
-      request_queue->shared_lock_holders_.insert(queue_iter->txn_id_);
-      queue_iter = request_queue->request_queue_.erase(queue_iter);
+      if (request_queue->exclusive_lock_holder_id_ == INVALID_TXN_ID) {
+        request_queue->shared_lock_holders_.insert(queue_iter->txn_id_);
+        queue_iter = request_queue->request_queue_.erase(queue_iter);
+      } else {
+        break;
+      }
     } else {
+      ExclusiveLockPreemptsSharedLock(request_queue, queue_iter->txn_id_);
       if (request_queue->shared_lock_holders_.empty()) {
         request_queue->exclusive_lock_holder_id_ = queue_iter->txn_id_;
         if (queue_iter->txn_id_ == request_queue->upgrading_) {
           request_queue->upgrading_ = INVALID_TXN_ID;
         }
         request_queue->request_queue_.erase(queue_iter);
-        break;
       }
+      break;
+    }
+  }
+}
+
+void LockManager::ExclusiveLockPreemptsSharedLock(LockRequestQueue *request_queue,
+                                                  const txn_id_t exclusive_lock_requester_id) {
+  auto shared_lock_iter = request_queue->shared_lock_holders_.begin();
+  while (shared_lock_iter != request_queue->shared_lock_holders_.end()) {
+    if (exclusive_lock_requester_id < *shared_lock_iter) {
+      Transaction *shared_lock_holder_transaction = TransactionManager::GetTransaction(*shared_lock_iter);
+      shared_lock_holder_transaction->SetState(TransactionState::ABORTED);
+      shared_lock_iter = request_queue->shared_lock_holders_.erase(shared_lock_iter);
+    } else {
+      shared_lock_iter++;
     }
   }
 }
